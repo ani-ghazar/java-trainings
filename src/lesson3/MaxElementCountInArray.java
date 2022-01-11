32e9f9cf8e0d0f945e2ab1161150d764a3b15fb8
ani-ghazar <Ghazaryan1!>
2022-01-12 03:39:51 +0400

    Updated logic of MaxElementCountInArray

--- src/lesson3/MaxElementCountInArray.java
+++ src/lesson3/MaxElementCountInArray.java
@@ -3,19 +3,24 @@ package lesson3;
 public class MaxElementCountInArray {
 
     public static void main(String[] args) {
-        int[] arr = {111, 12, 22, 98, 32, 23, 55, 111, 123, 45, 55, 45, 111};
+        int[] arr = {12, 22, 32, 23, 55, 111, 45, 45, 55, 45, 111};
 
         if (arr.length < 1) {
             System.out.print("Array is empty");
             return;
         }
         int max = arr[0];
-        int count = 0;
+        int count = 1;
         for (int i = 1; i < arr.length; i++) {
-            if(arr[i] > max) {
+            if(arr[i] >= max) {
+                if (arr[i] == max) {
+                    count++;
+                } else {
+                    count = 1;
+                }
                 max = arr[i];
-                count++;
             }
+
         }
 
         System.out.printf("Maximum element in array is " + max + ", which exists " + count + " times");

19332a5080d5489e48cfeb5b70f22fb94057fcb9
ani-ghazar <Ghazaryan1!>
2022-01-13 18:44:09 +0400

    Added logic for counting words in the sentence

---
+++ src/lesson4/CountOfWordsInSentence.java
@@ -0,0 +1,22 @@
+package lesson4;
+
+public class CountOfWordsInSentence {
+
+    public static void main(String[] args) {
+        String sentence = "When the Soviet Union collapsed in 1991 and Azerbaijan gained its independence," +
+                " one of the first laws passed in the new Parliament was the adoption of a new Latin-script alphabet." +
+                "Since 1992 (current version of the modern alphabet defined using the Latin script, " +
+                "replacing Ää with the historic Əə for better sorting)";
+
+        String[] splittedSentence = sentence.split(" ");
+        int length = splittedSentence.length;
+        int count = 0;
+        for (int i = 0; i < length; i++) {
+            if (!splittedSentence[i].matches("\\d.*\\d")) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
